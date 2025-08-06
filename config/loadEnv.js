01dc143b0dbfb30be4e0303d1afd76e6a43e6cd3
Kavindu Vidushan Karunathilaka <143307455+karuna3laka@users.noreply.github.com>
2025-08-06 06:37:48 +0530

    Initial commit with error handling middleware

---
+++ config/loadEnv.js
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Convert to CommonJS __dirname, __filename in ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Determine which .env file to load
+const nodeEnv = process.env.NODE_ENV || 'development';
+const envPath = resolve(__dirname, `../.env.${nodeEnv}.local`);
+
+// Load the .env file
+config({ path: envPath });
+
+console.log(`Loaded environment: ${nodeEnv}`);
+
+// ✅ Export values manually
+export const DB_URI = process.env.DB_URI;
+export const NODE_ENV = process.env.NODE_ENV;
+export const PORT = process.env.PORT;
