28676ee3bb4b019996ca5949f20dd9fe41b5f2c1
Kavindu Vidushan Karunathilaka <143307455+karuna3laka@users.noreply.github.com>
2025-08-11 15:47:51 +0530

    subscrption model implimeted !

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,15 +1,29 @@
+// controllers/subscription.controller.js
 import Subscription from '../models/subscription.model.js';
 
-export const createSubscription = async  (req, res, next) => {
+export const createSubscription = async (req, res, next) => {
     try {
-        const subscription = await Subscription.create( {
+        const subscription = await Subscription.create({
             ...req.body,
             user: req.user._id,
-
         });
-
-        res.status(201).json({ success : true , data : subscription });
-    } catch(e){
+        res.status(201).json({ success: true, data: subscription });
+    } catch (e) {
         next(e);
     }
-}
+};
+
+export const getUserSubscriptions = async (req, res, next) => {
+    try {
+        if (req.user.id != req.params.id) {
+            const error = new Error('You are not the owner of this account!');
+            error.status = 401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({ user: req.params.id });
+        res.status(200).json({ success: true, data: subscriptions });
+    } catch (e) {
+        next(e);
+    }
+};
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,19 +1,19 @@
 import { Router } from 'express';
-import authorize from '../middlewares/auth.middleware.js'
-import {createSubscription} from '../controllers/subscription.controller.js'
+import authorize from '../middlewares/auth.middleware.js';
+import { createSubscription, getUserSubscriptions } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => res.send( { title: 'GET all subscriptions' }));
+subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }));
 subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }));
 
-subscriptionRouter.post('/', authorize , createSubscription );
+subscriptionRouter.post('/', authorize, createSubscription);
 
 subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
 
 subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
 
-subscriptionRouter.get('/user/:id', (req, res) => res.send({ title: 'Get All Subscriptions for User' }));
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'Cancel Subscription' }));
 subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({ title: 'GET upcoming renewals' }));
 
